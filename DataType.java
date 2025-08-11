463f7d0199ec184a76002282b930839dd39dc263
Saloni-1385 <cccsaloni2023@gmail.com>
2025-08-11 11:42:20 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,22 @@
+public class DataType{
+    public static void main(String[] args){
+        int a = 5;
+        float b = 4.5f;
+        char c = 'H';
+        String s = "Hii";
+        boolean m = true;
+        short d = 233;
+        long l = 3243434545L;
+        double p = 34444;
+        int h = 33;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(m);
+        System.out.println(d);
+        System.out.println(l);
+        System.out.println(p);
+        System.out.println(h);
+    }
+}
---
+++ variable.java
@@ -0,0 +1,14 @@
+public class variable{
+    public static void main(String[] args){
+        int a = 5;
+        String Name = "Saloni" ;
+        System.out.println(a);
+        System.out.println(Name);
+        int num1 = 34;
+        int num2 = 38;
+        int sum = num1+num2;
+        System.out.println(sum);
+        System.out.println(num1+num2);
+        
+    }
+}
